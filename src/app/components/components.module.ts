762c1e569faae5ed763a34cf3e18814c089b03ec
francisco-1998 <francmarinc@gmail.com>
2020-10-13 11:41:41 -0500

    Creacion y diseño del searchComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    NavbarComponent
   ]
 })
 export class ComponentsModule { }
